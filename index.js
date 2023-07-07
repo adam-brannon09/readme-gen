485040f134f8bd4779f5ec65502742b02ab5925c
adambrannon09-jhKgEp <66850339+adam-brannon09@users.noreply.github.com>
2023-07-07 16:48:58 -0500

    finish index.js

--- index.js
+++ index.js
@@ -1,13 +1,83 @@
 // TODO: Include packages needed for this application
 // Imported required packages
 const fs = require('fs');
+const path = require('path');
 const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown.js');
+
+// TODO: Create an array of questions for user input
+const questions = [
+    {
+        name: 'title',
+        type: 'input',
+        message: 'What is the name of your project?',
+    },
+    {
+        name: 'contents',
+        type: 'input',
+        message: "List the Contents of your repo's README.",
+    },
+    {
+        name: 'description',
+        type: 'input',
+        message: 'Please write a detailed description of your project',
+    },
+    {
+        name: 'installation',
+        type: 'input',
+        message: 'How do you install the dependencies for your project?',
+        default: 'npm i',
+    },
+    {
+        name: 'license',
+        type: 'list',
+        message: 'What kind of license should be applied to your project?',
+        choices: ['MIT', 'APACHE 2.0', 'GPL 3.0', 'BSD 3', 'None'],
+    },
+    {
+        name: 'usage',
+        type: 'input',
+        message: 'What info does the user need about using your repo?',
+    },
+    {
+        name: 'contribute',
+        type: 'input',
+        message: 'How can a fellow programmer contribute to your project?',
+    },
+    {
+        name: 'test',
+        type: 'input',
+        message: 'How are tests run on your project?',
+        default: 'npm test',
+    },
+
+    {
+        name: 'github',
+        type: 'input',
+        message: 'What is your GitHub username?',
+    },
+    {
+        name: 'email',
+        type: 'input',
+        message: 'What is your email address?',
+    },
+
+
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFileSync(path.join(process.cwd(), fileName), data);
+    console.log('README.md has been generated!');
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function initialize() {
+    inquirer.prompt(questions).then((answers) => {
+        const markdownContent = generateMarkdown(answers);
+        writeToFile('README.md', markdownContent);
+      });
+}
 
 // Function call to initialize app
-init();
+initialize();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,19 +1,99 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license === 'MIT') {
+    return '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)';
+  } else if (license === 'APACHE 2.0') {
+    return '[![License: Apache 2.0](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)';
+  } else if (license === 'GPL 3.0') {
+    return '[![License: GPL 3.0](https://img.shields.io/badge/License-GPL%203.0-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)';
+  } else if (license === 'BSD 3') {
+    return '[![License: BSD 3](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)';
+  } else {
+    return '';
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license === 'MIT') {
+    return '[MIT](https://opensource.org/licenses/MIT)';
+  } else if (license === 'APACHE 2.0') {
+    return '[Apache 2.0](https://opensource.org/licenses/Apache-2.0)';
+  } else if (license === 'GPL 3.0') {
+    return '[GPL 3.0](https://www.gnu.org/licenses/gpl-3.0)';
+  } else if (license === 'BSD 3') {
+    return '[BSD 3-Clause](https://opensource.org/licenses/BSD-3-Clause)';
+  } else {
+    return '';
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license === 'None') {
+    return '';
+  } else {
+    return `## License
+
+This project is licensed under the ${renderLicenseLink(license)} license.`;
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  ${renderLicenseBadge(data.license)}
 
+  ## Table of Contents
+  
+  - [Description](#description)
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [License](#license)
+  - [Questions](#questions)
+  
+  ## Description
+  
+  ${data.description}
+  
+  ## Installation
+  
+  To install the dependencies for this project, run the following command:
+  
+  \`\`\`
+  ${data.installation}
+  \`\`\`
+  
+  ## Usage
+  
+  ${data.usage}
+  
+  ## Contributing
+  
+  ${data.contribute}
+  
+  ## Tests
+  
+  To run tests on this project, use the following command:
+  
+  \`\`\`
+  ${data.test}
+  \`\`\`
+  
+  ${renderLicenseSection(data.license)}
+  
+  ## Questions
+  
+  For any questions or inquiries, please contact me via GitHub or email:
+  
+  GitHub: [${data.github}](https://github.com/${data.github})
+  
+  Email: ${data.email}
 `;
 }
 
